2310bcf4ce6c7595031a9cc5eceb775dbd378a6a
Pietro <pietropnl02@gmail.com>
2021-03-26 21:29:34 -0300

    Teste

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,7 +9,7 @@
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 
-  <title>Login</title>
+  <title>Login Pietro</title>
 </head>
 
 <body>
